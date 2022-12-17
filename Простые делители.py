29933c0d497963db6e0d93826782f896ea704cef
Nassef1997 <113440503+Nassef1997@users.noreply.github.com>
2022-12-18 01:10:39 +0300

    Update Простые делители.py

--- Простые делители.py
+++ Простые делители.py
@@ -1,9 +1,9 @@
-x = int(input('Введите целое число'))
-num = 0
-for i in range(2, x+1):
-    if x % i == 0:
+a = int(input('Целое число'))
+num1 = 0
+for r in range(2, a+1):
+    if a % r == 0:
      for j in range(2, i):
-        if i % j == 0:
-            num += 1
-     if num == 0:
-         print(i)
+        if r % j == 0:
+            num1 += 1
+     if num1 == 0:
+         print(r)
